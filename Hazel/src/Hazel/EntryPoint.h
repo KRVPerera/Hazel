43c6e91852fd35cd87bc3e548aff735cfc8cee44
KRVPerera <rukshan.viduranga@gmail.com>
2023-05-01 14:49:48 +0530

    feat: added logging support

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -8,6 +8,12 @@ extern Hazel::Application* Hazel::CreateApplicaton();
 int main(int argc, char** argv) {
 	printf("Start Hazel engine\n");
 
+	Hazel::Log::Init();
+
+	HZ_CORE_INFO("Initialized Log!");
+	int a = 4;
+	HZ_WARN("Initialized Log! : {0}", a);
+
 	auto app = Hazel::CreateApplicaton();
 	app->Run();
 	delete app;
