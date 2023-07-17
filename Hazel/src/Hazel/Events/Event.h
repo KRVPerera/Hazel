48822a60f7eb1758ea7438cbedcff5e185f6aec6
KRVPerera <rukshan.viduranga@gmail.com>
2023-07-17 13:11:00 +0530

    feat: add mouse events

--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -17,14 +17,20 @@ namespace Hazel {
 		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
 	};
 
-	enum EventCatagory {
+	enum EventCategory {
 		None = 0,
-		EventCatagoryApplication = BIT(0),
-		EventCatagoryInput = BIT(1),
-		EventCatagoryKeyboard = BIT(2),
-		EventCatagoryMouse = BIT(3),
-		EventCatagoryMouseButton = BIT(4)
+		EventCategoryApplication = BIT(0),
+		EventCategoryInput = BIT(1),
+		EventCategoryKeyboard = BIT(2),
+		EventCategoryMouse = BIT(3),
+		EventCategoryMouseButton = BIT(4)
 	};
+
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+								virtual EventType GetEventType() const override { return GetStaticType(); }\
+								virtual const char* GetName() const override { return #type; }
+
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
 
 	class HAZEL_API Event
 	{
@@ -35,7 +41,7 @@ namespace Hazel {
 		virtual int GetCategoryFlags() const = 0;
 		virtual std::string ToString() const { return GetName(); }
 
-		inline bool IsInCategory(EventCatagory category) {
+		inline bool IsInCategory(EventCategory category) {
 			return GetCategoryFlags() & category;
 		}
 
