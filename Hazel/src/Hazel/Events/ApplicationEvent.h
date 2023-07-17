b6a40a21d76fc914851bc6fcb7e365c793e8abf3
KRVPerera <rukshan.viduranga@gmail.com>
2023-07-17 14:21:18 +0530

    feat: fix to string method in window resize event

--- Hazel/src/Hazel/Events/ApplicationEvent.h
+++ Hazel/src/Hazel/Events/ApplicationEvent.h
@@ -15,6 +15,13 @@ namespace Hazel {
 		inline unsigned int GetWidth() const { return m_Width; }
 		inline unsigned int GetHeight() const { return m_Height; }
 
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "WindowResizeEvent: " << m_Width << ", " << m_Height;
+			return ss.str();
+		}
+
 		EVENT_CLASS_TYPE(WindowResize)
 			EVENT_CLASS_CATEGORY(EventCategoryApplication);
 	private:
