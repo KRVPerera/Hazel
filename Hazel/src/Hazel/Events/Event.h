9e81ac4fc8bb6e5586bb726df11af36ec17bf179
KRVPerera <rukshan.viduranga@gmail.com>
2023-07-17 14:04:17 +0530

    feat: create event dispatcher

--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -51,7 +51,23 @@ namespace Hazel {
 
 	class EnevtDispatcher
 	{
+		template<typename T>
+		using EventFn = std::function<bool(T&)>;
 
+	public:
+		EnevtDispatcher(Event& event) : m_Event(event) { }
+
+		template<typename T>
+		bool Dispatch(EventFn<T> func) {
+			if (m_Event.GetEventType() == T::GetStaticType()) {
+				m_Event.m_Handled = func(*(T*)&m_Event);
+				return true;
+			}
+			return false;
+		}
+
+	private:
+		Event& m_Event;
 	};
 
 
