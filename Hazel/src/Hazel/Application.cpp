098e594c589c0c9fb4bbcf16bb540a3afe8e2a8c
KRVPerera <rukshan.viduranga@gmail.com>
2023-07-17 14:23:40 +0530

    test: event type negative test

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -17,7 +17,14 @@ namespace Hazel {
 	void Application::Run()
 	{
 		WindowResizeEvent e(1280, 720);
-		HZ_TRACE(e);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			HZ_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			HZ_TRACE(e);
+		}
 
 		while (true);
 	}
