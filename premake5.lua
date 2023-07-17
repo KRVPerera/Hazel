36f2f06b5c233f77f9c33bd8f7686bf3f71243bd
KRVPerera <rukshan.viduranga@gmail.com>
2023-07-17 20:18:41 +0530

    build: enable precompiled headers

--- premake5.lua
+++ premake5.lua
@@ -13,6 +13,9 @@ language "C++"
 targetdir("bin/" .. outputdir .. "/%{prj.name}")
 objdir("bin-int/" .. outputdir .. "/%{prj.name}")
 
+pchheader "hzpch.h"
+pchsource "Hazel/src/hzpch.cpp"
+
 files {"%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"}
 
 includedirs {"%{prj.name}/src", "%{prj.name}/vendor/spdlog/include"}
