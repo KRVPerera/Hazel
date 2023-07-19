1a78399fe55f1b920f9a8471b1102444cf8cd285
KRVPerera <rukshan.viduranga@gmail.com>
2023-07-19 14:32:23 +0530

    feat: add window interface

--- Hazel/Window.h
+++ Hazel/Window.h
@@ -20,4 +20,22 @@ namespace Hazel {
 	};
 
 	// Interface representing a desktop system based Window
+	class HAZEL_API Window
+	{
+	public:
+		using EventCallbackFn = std::function<void(Event&)>;
+
+		virtual ~Window() {}
+		virtual void OnUpdate() = 0;
+
+		virtual unsigned int GetWidth() const = 0;
+		virtual unsigned int GetHeight() const = 0;
+
+		// Window attributes
+		virtual void SetEventCallback(const EventCallbackFn& callback) = 0;
+		virtual void SetVSync(bool enabled) = 0;
+		virtual bool IsVSync() const = 0;
+
+		static Windw* Create(const WindowProps& props = WindowProps());
+	};
 }
